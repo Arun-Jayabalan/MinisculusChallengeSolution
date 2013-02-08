e6b3ec89dff3570bc966b9d2632d646c0d12413d
arun-jayabalan <akasthi@gmail.com>
2013-02-08 18:43:08 +0000

    Updated with Json client code -Arun

--- challenge.rb
+++ challenge.rb
@@ -1,5 +1,7 @@
 ##### MINISCULUS CHALLENGE - SOLUTION #######
 #### Using one of the http client  like fiddler, Dev http client or CURL get the responses  ####
+require 'rest-client'
+require 'json'
 
 $characterSet=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
@@ -8,17 +10,41 @@ $characterSet=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
                ".", ",", "?", "!", "'", "\"", " "]
 
+def submit_answer(answer,reference_url)
+
+  minisUri = "http://minisculuschallenge.com/"
+  reference_code = reference_url.split("/")
+  reference_url = minisUri << reference_code[2][0..-6]
+  body = {
+      answer:answer
+  }.to_json
+
+  response = RestClient.put reference_url, body, {:content_type => :json}
+  data=JSON.parse(response.body)
+
+  if data["code"] != nil
+    puts "Code :" << data["code"]
+    puts ""
+    return data["code"]
+  else
+    puts  "Question: " << data["question"]
+    puts "Reference Url: " << data["reference-url"]
+    puts ""
+    return [data["question"],data["reference-url"]]
+  end
+
+end
+
 def encoder_mark_i(first_wheel,messageToBeEncoded)
   messageToBeEncoded.split(//).map{|messageChar|$characterSet[($characterSet.index(messageChar)+first_wheel) % $characterSet.length]}.join
 end
 
-puts encoder_mark_i(6,"Strong NE Winds!")
 
 def encoder_mark_ii(first_wheel,second_wheel,messageToBeEncoded)
   messageToBeEncoded.split(//).map{|messageChar|$characterSet[(($characterSet.index(messageChar)+first_wheel) - (2 * second_wheel)) % $characterSet.length]}.join
 end
 
-puts encoder_mark_ii(9,3,"The Desert Fox will move 30 tanks to Calais at dawn")
+#puts encoder_mark_ii(9,3,"The Desert Fox will move 30 tanks to Calais at dawn")
 
 def encoder_mark_iv(first_wheel,second_wheel,messageToBeEncoded)
   encodedChars = messageToBeEncoded.split(//)
@@ -30,7 +56,6 @@ def encoder_mark_iv(first_wheel,second_wheel,messageToBeEncoded)
   encodedMessage
 end
 
-puts encoder_mark_iv(4,7,"The white cliffs of Alghero are visible at night")
 
 def decoder_for_mark_iv(first_wheel,second_wheel,messageToBeDecoded)
   encodedChars = messageToBeDecoded.split(//)
@@ -44,10 +69,6 @@ def decoder_for_mark_iv(first_wheel,second_wheel,messageToBeDecoded)
 
   decodedChars.join
 end
-
-puts decoder_for_mark_iv(7,2,"WZyDsL3u'0TfxP06RtSSF 'DbzhdyFIAu2 zF f5KE\"SOQTNA8A\"NCKPOKG5D9GSQE'M86IGFMKE6'K4pEVPK!bv83I")
-
-#print mark_IV_encoder(7,2,"The rockets will strike at coordinates 49.977984 7.9257857 422979.83 5536735.81 on Oct. 7th")
 
 
 def wheel_positions_and_message_decoder_for_mark_iv(encoded_message,clueWord)
@@ -64,6 +85,11 @@ def wheel_positions_and_message_decoder_for_mark_iv(encoded_message,clueWord)
   return [wheelPositions,decodedMessage]
 end
 
-print wheel_positions_and_message_decoder_for_mark_iv("QT4e8MJYVhkls.27BL9,.MSqYSi'IUpAJKWg9Ul9p4o8oUoGy'ITd4d0AJVsLQp4kKJB2rz4dxfahwUa\"Wa.MS!k4hs2yY3k8ymnla.MOTxJ6wBM7sC0srXmyAAMl9t\"Wk4hs2yYTtH0vwUZp4a\"WhB2u,o6.!8Zt\"Wf,,eh5tk8WXv9UoM99w2Vr4!.xqA,5MSpWl9p4kJ2oUg'6evkEiQhC'd5d4k0qA'24nEqhtAQmy37il9p4o8vdoVr!xWSkEDn?,iZpw24kF\"fhGJZMI8nkI","BUNKER")
-print "\n"
-#print wheel_positions_and_message_decoder_for_mark_iv("QT4e8MJYVhkls.27BL9,.MSqYSi'IUpAJKWg9Ul9p4o8oUoGy'ITd4d0AJVsLQp4kKJB2rz4dxfahwUa\"Wa.MS!k4hs2yY3k8ymnla.MOTxJ6wBM7sC0srXmyAAMl9t\"Wk4hs2yYTtH0vwUZp4a\"WhB2u,o6.!8Zt\"Wf,,eh5tk8WXv9UoM99w2Vr4!.xqA,5MSpWl9p4kJ2oUg'6evkEiQhC'd5d4k0qA'24nEqhtAQmy37il9p4o8vdoVr!xWSkEDn?,iZpw24kF\"fhGJZMI8nkI","FURLIN")
+nextQuestion , refUrl = submit_answer(encoder_mark_i(6,"Strong NE Winds!"),"/question/14f7ca5f6ff1a5afb9032aa5e533ad95.html")
+nextQuestion , refUrl = submit_answer(encoder_mark_ii(9,3,nextQuestion),refUrl)
+nextQuestion , refUrl = submit_answer(encoder_mark_iv(4,7,nextQuestion),refUrl)
+code = submit_answer(decoder_for_mark_iv(7,2,nextQuestion),refUrl)
+puts "****** Answer : [Wheel positions] <Answer> ************"
+print  wheel_positions_and_message_decoder_for_mark_iv(code,"BUNKER")
+puts ""
+puts "********************************************************"
